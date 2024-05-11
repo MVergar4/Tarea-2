1d2f35c3364cecc73416ff36be75ec68446db406
MVergar4 <marcvergara2023@udec.cl>
2024-05-11 17:08:46 -0400

    Añadida subclase ReunionVirtual

---
+++ Tarea2/src/main/java/org/example/ReunionVirtual.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ReunionVirtual extends Reunion {
+    private String enlace;
+    public ReunionVirtual(String link) {
+        super();
+        enlace = link;
+    }
+    public String getEnlace() {
+        return enlace;
+    }
+}
