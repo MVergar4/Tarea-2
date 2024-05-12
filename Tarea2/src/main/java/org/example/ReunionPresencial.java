5f99915a2493dfb7e54fda6e114a009504965054
MVergar4 <marcvergara2023@udec.cl>
2024-05-12 19:39:35 -0400

    Añadida estructura de clase Reunion

--- Tarea2/src/main/java/org/example/ReunionPresencial.java
+++ Tarea2/src/main/java/org/example/ReunionPresencial.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
 public class ReunionPresencial extends Reunion {
     private String sala;
-    public ReunionPresencial(String room) {
-        super();
+    public ReunionPresencial(String room, Date d, Instant t, Duration e) {
+        super(d, t, e);
         sala = room;
     }
     public String getSala() {
--- Tarea2/src/main/java/org/example/ReunionVirtual.java
+++ Tarea2/src/main/java/org/example/ReunionVirtual.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
 public class ReunionVirtual extends Reunion {
     private String enlace;
-    public ReunionVirtual(String link) {
-        super();
+    public ReunionVirtual(String link, Date d, Instant t, Duration e) {
+        super(d, t, e);
         enlace = link;
     }
     public String getEnlace() {
