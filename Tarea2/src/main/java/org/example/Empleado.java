24b58e8e4946c550cf3d89f0ffb80f32d533b809
pcfuenr <panchocifuentes1234@gmail.com>
2024-05-12 19:51:12 -0400

    Creado clase Empleado

---
+++ Tarea2/src/main/java/org/example/Empleado.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Empleado{
+    private String id;
+    private String apellido;
+    private String nombre;
+    private String correo;
+    public Empleado(String id, String apellido, String nombre, String correo){
+        this.id=id;this.apellido=apellido;this.nombre=nombre;this.correo=correo;
+    }
+}
