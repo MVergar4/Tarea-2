fe0cc59ec9a83ba7850486daa2884ff8aa5ab057
pcfuenr <panchocifuentes1234@gmail.com>
2024-05-12 21:10:19 -0400

    Clase Retraso + correjido error en asistencia

--- Tarea2/src/main/java/org/example/Asistencia.java
+++ Tarea2/src/main/java/org/example/Asistencia.java
@@ -5,7 +5,6 @@ public class Asistencia {
     private int NumAsistentes;
     private float PorcentajeAsistentes;
     private ArrayList<Empleado> Asistencias;
-    private ArrayList<Empleado> TotalAsistencias;
     private ArrayList<Empleado> Ausencias;
     private ArrayList<Empleado> Atrasados;
     public Asistencia(){}
