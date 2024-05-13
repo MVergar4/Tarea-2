c2ddda90115cb0a52f1bce3c64a5fd291432a430
pcfuenr <panchocifuentes1234@gmail.com>
2024-05-12 20:32:56 -0400

    Añadido clase Asistencia

---
+++ Tarea2/src/main/java/org/example/Asistencia.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.util.ArrayList;
+
+public class Asistencia {
+    private int NumAsistentes;
+    private float PorcentajeAsistentes;
+    private ArrayList<Empleado> Asistencias;
+    private ArrayList<Empleado> TotalAsistencias;
+    private ArrayList<Empleado> Ausencias;
+    private ArrayList<Empleado> Atrasados;
+    public Asistencia(){}
+}
