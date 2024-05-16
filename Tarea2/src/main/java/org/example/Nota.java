419717d4c42624e4f0f14a71ab707c05e43a267c
pcfuenr <panchocifuentes1234@gmail.com>
2024-05-16 05:02:35 -0400

    documentado Nota

--- Tarea2/src/main/java/org/example/Nota.java
+++ Tarea2/src/main/java/org/example/Nota.java
@@ -1,10 +1,20 @@
 package org.example;
 
+/**
+ * Clase que sirve para las notas que se hacen en la reunion
+ */
 public class Nota {
+    /** Contenido de la nota */
     private String contenido;
+    /** Constructor de la nota que deja el contenido en blanco */
     public Nota() {
         contenido = "";
     }
+
+    /**
+     * metodo que sirve para añadir una nota
+     * @param s Mensaje que se quiere guardar
+     */
     public void añadirNota(String s) {
         if (contenido.isBlank()) {
             contenido = contenido + s + ".";
@@ -12,6 +22,11 @@ public class Nota {
             contenido = contenido + " " + s + ".";
         }
     }
+
+    /**
+     * getter del contenido de la nota
+     * @return contenido de la nota
+     */
     public String leerNota() {
         return contenido;
     }
