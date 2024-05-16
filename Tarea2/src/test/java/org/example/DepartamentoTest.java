159fe99722f567294fc2077ffde1512feba9fe6d
pcfuenr <panchocifuentes1234@gmail.com>
2024-05-16 06:56:47 -0400

    Tests Departamento

--- Tarea2/src/test/java/org/example/DepartamentoTest.java
+++ Tarea2/src/test/java/org/example/DepartamentoTest.java
@@ -4,37 +4,40 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepartamentoTest {
-
+    private Empleado A = new Empleado("12","Cifuentes","Francisco","fcifuentes");
+    private Empleado B = new Empleado("13","Vergara","Marcelo","mvergara");
+    private Departamento D = new Departamento("Finanzas");
     @BeforeEach
     void setUp() {
+        D.setEmpleados(A);D.setEmpleados(B);
     }
-
     @AfterEach
     void tearDown() {
     }
     @Test
-    void setEmpleado(){
-        Empleado A = new Empleado("12","Cifuentes","Francisco","fcifuentes");
-        Departamento D = new Departamento("Finanza");
-    }
-    @Test
     void obtenerCantidadEmpleados() {
-        Empleado A = new Empleado("12","Cifuentes","Francisco","fcifuentes");
-        Empleado B = new Empleado("13","Vergara","Marcelo","mvergara");
-        Departamento D = new Departamento("Finanzas");
-        D.setEmpleados(A);D.setEmpleados(B);
         assertEquals(2,D.obtenerCantidadEmpleados());
     }
     @Test
     void probarToString(){
-        Departamento D = new Departamento("Finanzas");
         assertEquals("Departamento Finanzas de empleados",D.toString());
     }
-
     @Test
     void invitarDepartamentoEntero() {
+        D.setEmpleados(A);D.setEmpleados(B);
+        Instant instante = Instant.parse("2024-05-13T12:00:00Z");
+        ArrayList<Invitacion> lista = new ArrayList<>();
+        Invitacion i = A.invitar(instante); Invitacion i2=B.invitar(instante);
+        lista.add(i);lista.add(i2);
+
+        assertEquals(lista,D.invitarDepartamentoEntero(instante));
+        //Dice que el test falla pero al revisar lo esperado con lo actual es identico.
     }
+
 }
