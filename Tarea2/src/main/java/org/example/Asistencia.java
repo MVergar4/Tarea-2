642077b863abfc148c704defa6d9cccf17666d89
pcfuenr <panchocifuentes1234@gmail.com>
2024-05-16 04:58:43 -0400

    documentado Asistencia

--- Tarea2/src/main/java/org/example/Asistencia.java
+++ Tarea2/src/main/java/org/example/Asistencia.java
@@ -1,14 +1,31 @@
 package org.example;
 import java.util.ArrayList;
 
+/**
+ * Clase que sirve para medir la asistencia en la reunion
+ */
 public class Asistencia {
+    /** Un empleado */
     protected Empleado empleado;
+    /**
+     * Constructor que recibe y guarda un empleado
+     * @param e Empleado
+     */
     public Asistencia(Empleado e) {
         empleado = e;
     }
+    /**
+     * getter del empleado
+     * @return el empleado
+     */
     public Empleado getEmpleado() {
         return empleado;
     }
+    /**
+     * Sobreescribe el toString para dar cuenta de lo que tiene cada objeto de esta clase
+     * @return frase con la id del empleado diciendo que esta presente
+     */
+    @Override
     public String toString() {
         return "Empleado " + empleado.getId() + ": presente.";
     }
