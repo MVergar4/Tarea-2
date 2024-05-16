194a9c631aed9299a2edf77dd9fae29bc7cadc20
pcfuenr <panchocifuentes1234@gmail.com>
2024-05-16 05:00:02 -0400

    Documentada la interfaz

--- Tarea2/src/main/java/org/example/Invitable.java
+++ Tarea2/src/main/java/org/example/Invitable.java
@@ -2,6 +2,9 @@ package org.example;
 
 import java.time.Instant;
 
+/**
+ * Interfaz que sirve para que tanto departamento como un solo empleado puedan ser invitados.
+ */
 public interface Invitable {
     public Invitacion invitar(Instant hora);
 }
